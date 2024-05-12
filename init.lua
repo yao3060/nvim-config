6b08a33a6d3ca6d65a8562fc9e83aff8548be631
YAO.Gmail <yao3060@gmail.com>
2024-05-12 12:16:00 +0800

    init

---
+++ init.lua
@@ -0,0 +1,33 @@
+require('options') 
+require('keymaps') 
+require('plugins') 
+
+
+local opt = {
+  noremap = true,
+  silent = true,
+}
+
+-- 本地变量
+local map = vim.api.nvim_set_keymap
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
+
+
+
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+-- vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+-- nvim-tree
+map("n", "<A-m>", ":NvimTreeToggle<CR>", opt)
+map("n", "<leader>m", ":NvimTreeToggle<CR>", opt)
